25684ae3a106f38eabd45e05db1ac4b687696e3b
Ishan <axil.ishan3@gmail.com>
2020-06-14 20:55:17 +0530

    Setting up openCV sdk

---
+++ app/src/main/java/com/opencv/project/MainActivity.java
@@ -0,0 +1,20 @@
+package com.opencv.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug())
+            Toast.makeText(this, "OpenCV loaded successfully", Toast.LENGTH_SHORT).show();
+    }
+}
